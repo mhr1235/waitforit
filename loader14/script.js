07a7ae9d6f20623551fef99a6cd99f0a87edeed7
Mark Ramos <65504141+mhr1235@users.noreply.github.com>
2023-09-03 12:17:24 -0400

    Update script.js

--- loader14/script.js
+++ loader14/script.js
@@ -1,7 +1,31 @@
-(function ($) {
+/*(function ($) {
   $(function () {
 
     
 
   });
-})(jQuery);
+})(jQuery);*/
+
+var x = window.matchMedia("(orientation:portrait)")
+
+if (x.matches) { // If media query matches
+      var myDiv = $("html, body");
+var scrollto = myDiv.offset().top + (myDiv.height() / 8);
+myDiv.animate({ scrollTop:  scrollto});
+  } else {
+     var myDiv = $("html, body");
+var scrollto = myDiv.offset().top + (myDiv.height() / 4);
+myDiv.animate({ scrollTop:  scrollto});
+  }
+ $(document).ready(function() {
+
+
+            
+                /*$("html, body").animate({
+                    scrollTop: $(
+                      'html, body').get(0).scrollHeight
+                }, 2000);*/
+
+
+            
+        });
