140acfb0eeb9d7377499a9f1c266d538a89294c0
Mark Ramos <65504141+mhr1235@users.noreply.github.com>
2023-09-10 18:38:11 -0400

    Add files via upload

--- loader12/script.js
+++ loader12/script.js
@@ -3,6 +3,60 @@ let checkbox1 = document.getElementById("order1");
 let checkbox2 = document.getElementById("order2");
 let checkbox3 = document.getElementById("order3");
 
+let menu = document.getElementById("top-nav-toggle");
+let bell = document.getElementById("bell");
+let bell2 = document.getElementById("bell2");
+
+
+
+
+const links = [];
+links[0]= "../loader2/index.html";
+links[1]= "../loader3/index.html";
+links[2]= "../loader4/index.html";
+links[3]= "../loader5/index.html";
+links[4]= "../loader1/index.html";
+links[5]= "../loader6/index.html";
+links[6]= "../loader7/index.html";
+links[7]= "../loader8/index.html";
+links[8]= "../loader9/index.html";
+links[9]= "../loader10/index.html";
+links[10]= "../loader11/index.html";
+links[11]= "../loader13/index.html";
+links[12]= "../loader14/index.html";
+links[13]= "../loader15/index.html";
+links[14]= "../loader16/index.html";
+links[15]= "../loader17/index.html";
+links[16]= "../loader18/index.html";
+links[17]= "../loader19/index.html";
+links[18]= "../loader20/index.html";
+
+function loadNew() {
+
+let random_url = Math.floor(Math.random() * 19); 
+
+ //console.log(random_url);
+  location.href = links[random_url];
+}
+
+menu.addEventListener("click", () => {
+  
+
+  loadNew();
+})
+
+ bell.addEventListener("click", () => {
+  
+
+  loadNew();
+})
+
+ bell2.addEventListener("click", () => {
+  
+
+  loadNew();
+})
+
 let box1;
 //let line= document.getElementsByClassName("order-track-status");
 
